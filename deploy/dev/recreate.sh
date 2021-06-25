1f693ea915baa4f7dc9004b16152aa606aefce6e
Vu Nguyen <aqaurius6666@gmail.com>
2021-06-25 22:00:15 +0700

    a

--- deploy/dev/recreate.sh
+++ deploy/dev/recreate.sh
@@ -1,5 +1,3 @@
 ./remove.sh
-docker-compose --env-file .env -f docker-dev.yml up --build -d --force-recreate -V
-source .env
-ssh -oStrictHostKeyChecking=no -p 22222 -R api.${SERVER_DOMAIN}:80:localhost:${SERVER_PORT} dev.nftal.io | \
+docker-compose --env-file .env -f docker-nodemon.yaml up --build -d --force-recreate -V
 ./log.sh
--- deploy/dev/recreate2.sh
+++
@@ -1,3 +0,0 @@
-./remove.sh
-docker-compose --env-file .env -f docker-nodemon.yaml up --build -d --force-recreate -V
-./log.sh
